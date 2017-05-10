7cd49335b909f83b9c78bd1a80d670c7f923f0cc
Scott Ranger <millenniumnight@googlemail.com>
2017-05-10 20:23:57 +0100

    Post install build script

--- src/index.js
+++ src/index.js
@@ -6,46 +6,42 @@ import { readJsonFileAsync, fileExistsAsync, readDirAsync } from './async-fs';
 import { getAllDependenciesForProject, getPackageJsonForDirectory } from './io';
 
 export const run = async () => {
-	try {
-		const cwd = process.cwd();
-		const deps = await getAllDependenciesForProject(cwd);
+	const cwd = process.cwd();
+	const deps = await getAllDependenciesForProject(cwd);
 
-		const dirToCheck = resolve(cwd, 'flow-typed/npm');
-		const typedFiles = await readDirAsync(dirToCheck);
+	const dirToCheck = resolve(cwd, 'flow-typed/npm');
+	const typedFiles = await readDirAsync(dirToCheck);
 
-		const promises = Object.keys(deps).map(async (dep, index) => {
-			const path = resolve(cwd, `node_modules/${dep}`);
-			const packageJson = await getPackageJsonForDirectory(path);
-			const mainFile = resolve(path, packageJson.main || 'index.js');
+	const promises = Object.keys(deps).map(async (dep, index) => {
+		const path = resolve(cwd, `node_modules/${dep}`);
+		const packageJson = await getPackageJsonForDirectory(path);
+		const mainFile = resolve(path, packageJson.main || 'index.js');
 
-			//search for .js.flow
-			const defFile = mainFile + '.flow';
-			const defFileExists = await fileExistsAsync(defFile);
+		//search for .js.flow
+		const defFile = mainFile + '.flow';
+		const defFileExists = await fileExistsAsync(defFile);
 
-			if (defFileExists) {
+		if (defFileExists) {
+			return { [dep]: 'typed' };
+		} else {
+			// check for flow typed defFile
+			const typedFileExists = typedFiles.some(filename => {
+				return filename.match(`^${dep}_v`);
+			});
+
+			if (typedFileExists) {
 				return { [dep]: 'typed' };
 			} else {
-				// check for flow typed defFile
-				const typedFileExists = typedFiles.some(filename => {
-					return filename.match(`^${dep}_v`);
-				});
-
-				if (typedFileExists) {
-					return { [dep]: 'typed' };
-				} else {
-					return { [dep]: 'untyped' };
-				}
+				return { [dep]: 'untyped' };
 			}
-		});
+		}
+	});
 
-		const results = await Promise.all(promises);
+	const results = await Promise.all(promises);
 
-		const summary = Object.assign({}, ...results);
+	const summary = Object.assign({}, ...results);
 
-		return summary;
-	} catch (e) {
-		console.log(e);
-	}
+	return summary;
 
 	return {};
 };
