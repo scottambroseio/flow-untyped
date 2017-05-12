efbb099077c15c412f5ccfd48de5f26041e76afa
Scott Ranger <millenniumnight@googlemail.com>
2017-05-12 17:41:08 +0100

    Output now prints the dependancy version

--- dist/index.js
+++ dist/index.js
@@ -15,28 +15,30 @@ var getInfo = exports.getInfo = async function getInfo(cwd, projectDeps) {
 	var flowTypedDir = (0, _path.resolve)(cwd, 'flow-typed/npm');
 	var flowTypeDefs = await (0, _asyncFs.readDirAsync)(flowTypedDir);
 
-	return Promise.all(Object.keys(projectDeps).map(async function (dep, index) {
+	return Promise.all(Object.keys(projectDeps).map(async function (dep) {
 		var path = (0, _path.resolve)(cwd, 'node_modules/' + dep);
 		var packageJsonPath = (0, _path.resolve)(path, 'package.json');
 
 		var _ref = await (0, _asyncFs.readJsonFileAsync)(packageJsonPath),
-		    main = _ref.main;
+		    main = _ref.main,
+		    version = _ref.version;
 
 		var mainFile = (0, _path.resolve)(path, main || 'index.js');
 
 		// if main file is index.js, look for index.js.flow
 		var defFile = mainFile + '.flow';
 		var defFileExists = await (0, _asyncFs.fileExistsAsync)(defFile);
+		var depString = dep + '@' + version;
 
 		if (defFileExists) {
-			return dep + ' is typed';
+			return depString + ' is typed';
 		} else {
 			// check for flow-typed defFile in flow-typed directory
 			var flowTypeDefExists = flowTypeDefs.some(function (filename) {
 				return filename.match('^' + dep + '_v');
 			});
 
-			return flowTypeDefExists ? dep + ' is typed' : dep + ' is untyped';
+			return flowTypeDefExists ? depString + ' is typed' : depString + ' is untyped';
 		}
 	}));
 };
--- src/index.js
+++ src/index.js
@@ -11,25 +11,26 @@ export const getInfo = async (cwd: string, projectDeps: Object) => {
 	const flowTypedDir = resolve(cwd, 'flow-typed/npm');
 	const flowTypeDefs = await readDirAsync(flowTypedDir);
 
-	return Promise.all(Object.keys(projectDeps).map(async (dep, index) => {
+	return Promise.all(Object.keys(projectDeps).map(async (dep) => {
 		const path = resolve(cwd, `node_modules/${dep}`);
 		const packageJsonPath = resolve(path, 'package.json');
-		const { main } = await readJsonFileAsync(packageJsonPath);
+		const { main, version } = await readJsonFileAsync(packageJsonPath);
 		const mainFile = resolve(path, main || 'index.js');
 
 		// if main file is index.js, look for index.js.flow
 		const defFile = mainFile + '.flow';
 		const defFileExists = await fileExistsAsync(defFile);
+		const depString = `${dep}@${version}`
 
 		if (defFileExists) {
-			return `${dep} is typed`;
+			return `${depString} is typed`;
 		} else {
 			// check for flow-typed defFile in flow-typed directory
 			const flowTypeDefExists = flowTypeDefs.some(filename => {
 				return filename.match(`^${dep}_v`);
 			});
 
-			return flowTypeDefExists ? `${dep} is typed` : `${dep} is untyped`;
+			return flowTypeDefExists ? `${depString} is typed` : `${depString} is untyped`;
 		}
 	}));
 };
